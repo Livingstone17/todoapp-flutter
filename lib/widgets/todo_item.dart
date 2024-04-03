c4fe81237888ff57c2fbd75949ec02858544e773
Livingstone17 <peterson.omobolaji@gmail.com>
2024-04-03 22:48:02 +0100

    UI update

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -8,14 +8,37 @@ class TodoItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: ListTile(
-        onTap: () {},
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        onTap: () {
+          print("TO do item clicked");
+        },
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
         tileColor: Colors.white,
         leading: Icon(
           Icons.check_box,
           color: tdBlue,
         ),
-        title: Text("Call Ramana"),
+        title: Text(
+          "Call Ramana",
+          style: TextStyle(
+              fontSize: 16,
+              color: tdBlack,
+              decoration: TextDecoration.lineThrough),
+        ),
+        trailing: Container(
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.symmetric(vertical: 12),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+              color: tdRed, borderRadius: BorderRadius.circular(5)),
+          child: IconButton(
+            color: Colors.white,
+            iconSize: 18,
+            icon: Icon(Icons.delete),
+            onPressed: () {},
+          ),
+        ),
       ),
     );
   }
